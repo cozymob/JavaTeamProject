6f4bccfaa8008de81695fa3662621253ff36e4e8
taile9 <163844880+taile9@users.noreply.github.com>
2025-06-02 01:14:02 +0900

    Update QuizLoader.java

--- QuizLoader.java
+++ QuizLoader.java
@@ -3,25 +3,40 @@ import java.util.*;
 
 public class QuizLoader {
     public static List<Quiz> loadFromFile(String filename) throws IOException {
-        List<Quiz> quizzes = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        String line;
-        String number = "", question = "", answer = "", explanation = "";
+        //리스트 만들기
+    	List<Quiz> quizzes = new ArrayList<>();
 
-        while ((line = br.readLine()) != null) {
-            line = line.trim();
-            if (line.startsWith("[") && line.endsWith("]")) {
-                number = line.substring(1, line.length() - 1);
-            } else if (line.startsWith("Q:")) {
-                question = line.substring(2).trim();
-            } else if (line.startsWith("A:")) {
-                answer = line.substring(2).trim();
-            } else if (line.startsWith("E:")) {
-                explanation = line.substring(2).trim();
-                quizzes.add(new Quiz(number, question, answer, explanation));
+    	try {
+    		//줄 단위로 파일 읽기
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            String number = "", question = "", answer = "", explanation = "";
+            //한 줄씩 line으로 받기
+            while ((line = br.readLine()) != null) {
+                line = line.trim(); // 앞뒤 공백 제거
+                //문제 번호 [1.1] -> 1.1
+                if (line.startsWith("[") && line.endsWith("]")) {
+                    number = line.substring(1, line.length() - 1); 
+                 // Q:문제내용
+                } else if (line.startsWith("Q:")) {
+                    question = line.substring(2).trim(); 
+                 // A:O 또는 X
+                } else if (line.startsWith("A:")) {
+                    answer = line.substring(2).trim(); 
+                 // E:해설
+                } else if (line.startsWith("E:")) {
+                    explanation = line.substring(2).trim(); 
+                    //퀴즈에 번호, 문제, 답, 해설 저장
+                    quizzes.add(new Quiz(number, question, answer, explanation));
+                }
             }
+            //파일 닫기
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("파일 오류 발생" + e.getMessage());
         }
-        br.close();
+   
         return quizzes;
     }
 }
