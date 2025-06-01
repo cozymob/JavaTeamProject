f2ff375c07717d7e42bdeb3a007106f5d99d4c70
taile9 <163844880+taile9@users.noreply.github.com>
2025-06-02 00:24:00 +0900

    Update AnswerRecord.java

--- AnswerRecord.java
+++ AnswerRecord.java
@@ -6,11 +6,16 @@ public class AnswerRecord {
         this.quiz = quiz;
         this.userAnswer = userAnswer;
     }
-
+    
+    //퀴즈 정답이 유저 답과 같은지 비교(답이 맞았는지 틀렸는지)
     public boolean isCorrect() {
         return quiz.getAnswer().equalsIgnoreCase(userAnswer);
     }
-
-    public Quiz getQuiz() { return quiz; }
-    public String getUserAnswer() { return userAnswer; }
+    //퀴즈 전체 들고오기
+    public Quiz getQuiz() { 
+    	return quiz; }
+    
+    //유저 답 들고오기
+    public String getUserAnswer() { 
+    	return userAnswer; }
 }
